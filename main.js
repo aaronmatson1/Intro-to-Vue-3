c66746f10068089498d24091aab6450bd34fed22
Aaron Matson <amatson@ladders.com>
2021-09-29 10:00:44 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'These socks are super comfortable!',
+            image: './assets/images/socks_green.jpg',
+            url: 'http://corndog.io/',
+            
         }
     }
 })
